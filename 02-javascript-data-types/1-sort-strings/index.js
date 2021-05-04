63f74b5c5c04b4dd8f58b0aadde4460227e891da
anatoli.mironau <anatoli.mironau@solarwinds.com>
2021-05-04 18:16:55 +0300

    Implemented task 02-javascript-data-types/1-sort-strings
    Tested:
    Locally

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,9 +1,29 @@
+const direction = {
+  asc: 'asc',
+  desc: 'desc',
+};
+
+const localeCompareSort = (a, b) => a.localeCompare(b, 'ru', {caseFirst: 'upper'});
+
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = direction.asc) {
+  const sortedArray = arr.slice();
 
+  switch (param) {
+    case direction.asc:
+      sortedArray.sort(localeCompareSort);
+      break;
+    case direction.desc:
+      sortedArray.sort(localeCompareSort).reverse();
+      break;
+    default:
+      console.error(`Wrong sorting type: ${param}`);
+  }
+
+  return sortedArray;
 }
