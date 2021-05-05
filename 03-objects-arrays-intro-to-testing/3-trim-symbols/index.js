3ae4fdadf47e072ce82501c883076e08661b1838
anatoli.mironau <anatoli.mironau@solarwinds.com>
2021-05-05 19:11:52 +0300

    Implemented task 02-javascript-data-types/3-trim-symbols
    Tested:
    Locally

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  const newStringArray = [];
+  const separator = '';
+  let counter;
+  let previousSymbol;
 
+  string.split(separator).map(symbol => {
+    if (size !== 0 && (counter < size || previousSymbol !== symbol || !size)) {
+      if (previousSymbol !== symbol) {
+        counter = 0;
+      }
+
+      newStringArray.push(symbol);
+      previousSymbol = symbol;
+      counter++;
+    } else {
+      counter++;
+    }
+  });
+  
+  return newStringArray.join(separator);
 }
