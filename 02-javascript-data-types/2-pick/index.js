fdf578ab728439197e154817019800c70455a709
anatoli.mironau <anatoli.mironau@solarwinds.com>
2021-05-05 13:02:28 +0300

    Implemented task 02-javascript-data-types/3-omit
    - added task #3;
    - refactoring of task #2;
    
    Tested:
    Locally

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -1,15 +1,27 @@
 /**
- * pick - Creates an object composed of the picked object properties:
- * @param {object} obj - the source object
- * @param {...string} fields - the properties paths to pick
- * @returns {object} - returns the new object
+ * Get new object based on should it include properties from initial or not
+ * @param {boolean} isPeak - is it pick (true) or omit (false) operation
+ * @param {Object} obj
+ * @param {...string} fields
+ * @returns {Object}
  */
-export const pick = (obj, ...fields) => {
+export const getNewObject = (isPeak, obj, ...fields) => {
   const newObj = {};
   for (const [key, value] of Object.entries(obj)) {
-    if (fields.includes(key)) {
+    if (fields.includes(key) && isPeak) {
+      newObj[key] = value;
+    } else if (!fields.includes(key) && !isPeak) {
       newObj[key] = value;
     }
   }
   return newObj;
 };
+
+
+/**
+ * pick - Creates an object composed of the picked object properties:
+ * @param {object} obj - the source object
+ * @param {...string} fields - the properties paths to pick
+ * @returns {object} - returns the new object
+ */
+export const pick = (obj, ...fields) => getNewObject(true, obj, ...fields);
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -1,9 +1,9 @@
+import { getNewObject } from "../2-pick";
+
 /**
  * omit - creates an object composed of enumerable property fields
  * @param {object} obj - the source object
  * @param {...string} fields - the properties paths to omit
  * @returns {object} - returns the new object
  */
-export const omit = (obj, ...fields) => {
-
-};
+export const omit = (obj, ...fields) => getNewObject(false, obj, ...fields);
